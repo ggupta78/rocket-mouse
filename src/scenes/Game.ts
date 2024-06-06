2ded907171bfbef47a38406c20a66c66a6f00e3e
Gautam Gupta <ggupta78@gmail.com>
2024-06-06 17:47:37 +0800

    add Game scene and background

---
+++ src/scenes/Game.ts
@@ -0,0 +1,18 @@
+import Phaser from "phaser";
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super("game");
+  }
+
+  preload() {
+    this.load.image("background", "house/bg_repeat_340x640.png");
+  }
+
+  create() {
+    const width = this.scale.width;
+    const height = this.scale.height;
+
+    this.add.tileSprite(0, 0, width, height, "background").setOrigin(0, 0);
+  }
+}
