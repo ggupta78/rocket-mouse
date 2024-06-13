e354a37654e115b246a1c5d99a982e6a6b1b13db
Gautam Gupta <ggupta78@gmail.com>
2024-06-13 12:26:42 +0800

    complete Running and Scrolling

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,6 +4,9 @@ import TextureKeys from "../consts/TextureKeys";
 import AnimationKeys from "../consts/AnimationKeys";
 
 export default class Game extends Phaser.Scene {
+  private background!: Phaser.GameObjects.TileSprite;
+  private body!: Phaser.Physics.Arcade.Body;
+
   constructor() {
     super(SceneKeys.Game);
   }
@@ -14,17 +17,42 @@ export default class Game extends Phaser.Scene {
     const width = this.scale.width;
     const height = this.scale.height;
 
-    this.add
+    this.background = this.add
       .tileSprite(0, 0, width, height, TextureKeys.Background)
-      .setOrigin(0, 0);
+      .setOrigin(0, 0)
+      .setScrollFactor(0, 0);
 
-    this.add
+    const mouse = this.physics.add
       .sprite(
         width * 0.5,
-        height * 0.5,
+        height - 30,
         TextureKeys.RocketMouse,
         "rocketmouse_run01.png"
       )
+      .setOrigin(0.5, 1)
       .play(AnimationKeys.RocketMouseRun);
+
+    // const body = mouse.body as Phaser.Physics.Arcade.Body;
+    this.body = mouse.body as Phaser.Physics.Arcade.Body;
+    this.body.setCollideWorldBounds(true);
+
+    this.body.setVelocityX(200);
+
+    this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height - 30);
+
+    this.cameras.main.startFollow(mouse);
+    this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height);
+  }
+
+  update(time: number, delta: number): void {
+    //scroll the background
+    // let cameraScrollX = this.cameras.main.scrollX;
+    // let mouseX = this.body.x;
+    // let tilePositionX = this.background.tilePositionX;
+    // console.log(
+    //   `mouseX: ${mouseX}, cameraScrollX: ${cameraScrollX}, tilePositionX: ${tilePositionX}`
+    // );
+    this.background.setTilePosition(this.cameras.main.scrollX);
+    // console.log(`New TitlePositionX: ${this.background.tilePositionX}`);
   }
 }
