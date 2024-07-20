7a90975c9dc7359b046a7e96916598e4606cd129
Gautam Gupta <ggupta78@gmail.com>
2024-07-20 12:54:41 +0530

    complete Decorating the house - Class it up with bookcases

--- src/consts/TextureKeys.ts
+++ src/consts/TextureKeys.ts
@@ -1,6 +1,11 @@
 enum TextureKeys {
   Background = "background",
+  Bookcase1 = "bookcase-1",
+  Bookcase2 = "bookcase-2",
+  MouseHole = "mouse-hole",
   RocketMouse = "rocket-mouse",
+  Window1 = "window-1",
+  Window2 = "window-2",
 }
 
 export default TextureKeys;
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -11,6 +11,17 @@ export default class Preloader extends Phaser.Scene {
   preload() {
     this.load.image(TextureKeys.Background, "house/bg_repeat_340x640.png");
 
+    // Load Mouse Hole
+    this.load.image(TextureKeys.MouseHole, "house/object_mousehole.png");
+
+    // Load Windows
+    this.load.image(TextureKeys.Window1, "house/object_window1.png");
+    this.load.image(TextureKeys.Window2, "house/object_window2.png");
+
+    // Load Bookcases
+    this.load.image(TextureKeys.Bookcase1, "house/object_bookcase1.png");
+    this.load.image(TextureKeys.Bookcase2, "house/object_bookcase2.png");
+
     this.load.atlas(
       TextureKeys.RocketMouse,
       "characters/rocket-mouse.png",
